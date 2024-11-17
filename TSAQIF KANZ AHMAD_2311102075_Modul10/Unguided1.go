1ca68f33d9a540f28c6a7e9ea470100395cd64e0
TSAQIFKANZ <162876126+TSAQIFKANZ@users.noreply.github.com>
2024-11-17 23:00:12 +0700

    Add files via upload

---
+++ TSAQIF KANZ AHMAD_2311102075_Modul10/Unguided1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Mendefinisikan tipe array untuk menyimpan data berat balita
+type arrBalita [100]float64
+
+// Fungsi untuk menghitung nilai minimum dan maksimum
+func hitungMinMax(arrBerat arrBalita, n int, bMin, bMax *float64) {
+	*bMin = arrBerat[0]
+	*bMax = arrBerat[0]
+	for i := 1; i < n; i++ {
+		if arrBerat[i] < *bMin {
+			*bMin = arrBerat[i]
+		}
+		if arrBerat[i] > *bMax {
+			*bMax = arrBerat[i]
+		}
+	}
+}
+
+// Fungsi untuk menghitung rata-rata berat balita
+func rataRata(arrBerat arrBalita, n int) float64 {
+	var total float64
+	for i := 0; i < n; i++ {
+		total += arrBerat[i]
+	}
+	return total / float64(n)
+}
+
+func main() {
+	var n int
+	var berat arrBalita
+	var bMin, bMax, rataan float64
+
+	fmt.Print("Masukan banyak data berat balita: ")
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		fmt.Printf("Masukan berat balita ke-%d: ", i+1)
+		fmt.Scan(&berat[i])
+	}
+
+	// Memanggil fungsi untuk menghitung nilai minimum dan maksimum
+	hitungMinMax(berat, n, &bMin, &bMax)
+	// Menghitung rata-rata berat balita
+	rataan = rataRata(berat, n)
+
+	// Menampilkan hasil
+	fmt.Printf("Berat balita minimum: %.2f kg\n", bMin)
+	fmt.Printf("Berat balita maksimum: %.2f kg\n", bMax)
+	fmt.Printf("Rerata berat balita: %.2f kg\n", rataan)
+}
